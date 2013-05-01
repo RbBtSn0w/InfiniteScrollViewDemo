2d6b52194523d77bc9bfc630e8b6dbec393e579d
RbBtSn0w <hamiltonsnow@gmail.com>
2013-05-01 21:12:12 +0800

    Initial Commit

---
+++ InfiniteScrollViewDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  InfiniteScrollViewDemo
+//
+//  Created by Snow on 5/1/13.
+//  Copyright (c) 2013 Snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
